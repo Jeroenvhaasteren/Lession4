e7493c3f6abda7ace2d9b23f6104898677eaad00
Jeroen van Haasteren <jeroenvhaasteren@gmail.com>
2017-01-23 11:56:01 +0100

    Add First Activity manually

---
+++ app/src/main/java/org/bts/atry/lession4/MainActivity.java
@@ -0,0 +1,23 @@
+package org.bts.atry.lession4;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by Jeroen on 23/01/2017.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+}
