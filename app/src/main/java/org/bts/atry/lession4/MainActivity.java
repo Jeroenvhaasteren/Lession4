fbade4d8bfe6a9f2d0f2622ac49a70ee2c8f5bb4
Jeroen van Haasteren <jeroenvhaasteren@gmail.com>
2017-01-25 09:13:31 +0100

    Commit Lesson 23 januari 3 buttons added

--- app/src/main/java/org/bts/atry/lession4/MainActivity.java
+++ app/src/main/java/org/bts/atry/lession4/MainActivity.java
@@ -1,9 +1,11 @@
 package org.bts.atry.lession4;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 /**
@@ -15,9 +17,35 @@ public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnStartActivity = (Button) findViewById(R.id.btn_start_activity);
+        Button btnMainActivity2 = (Button) findViewById(R.id.btn_main_activity_2);
+        Button btnMainActivity3 = (Button) findViewById(R.id.btn_main_activity_3);
+
+        btnStartActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(MainActivity.TAG,"Button Clicked");
+                startSecondActivity(v);
+            }
+        });
+
+        btnMainActivity2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(MainActivity.TAG,"Button 2 Clicked");
+            }
+        });
+
+        btnMainActivity3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(MainActivity.TAG,"Button 3 Clicked");
+            }
+        });
 
         Toast.makeText(this, "I'am Created", Toast.LENGTH_SHORT).show();
         Log.d(MainActivity.TAG, "onCreate()");
@@ -70,4 +98,13 @@ public class MainActivity extends Activity {
         Toast.makeText(this, "I'am Restart", Toast.LENGTH_SHORT).show();
 
     }
+
+    public void startSecondActivity(View v) {
+
+        Intent activityIntent = new Intent(this, SecondActivity.class);
+        startActivity(activityIntent);
+
+    }
+
+
 }
