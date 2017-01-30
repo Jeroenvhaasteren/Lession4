308a01bf4ebe3bc0411812d6ace330bef8a26f6a
Jeroen van Haasteren <jeroenvhaasteren@gmail.com>
2017-01-30 11:37:54 +0100

    Commit Lesson 30 januari Send databack second activity

--- app/src/main/java/org/bts/atry/lession4/MainActivity.java
+++ app/src/main/java/org/bts/atry/lession4/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -16,54 +17,30 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private TextView mTvInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnStartActivity = (Button) findViewById(R.id.btn_start_activity);
-        Button btnMainActivity2 = (Button) findViewById(R.id.btn_main_activity_2);
-        Button btnMainActivity3 = (Button) findViewById(R.id.btn_main_activity_3);
-
-//        btnStartActivity.setOnClickListener(new OnClickClass());
-//        btnMainActivity2.setOnClickListener(new OnClickClass());
-//        btnMainActivity3.setOnClickListener(new OnClickClass());
+        final Button btnStartActivity = (Button) findViewById(R.id.btn_start_activity);
+        final Button btnMainActivity2 = (Button) findViewById(R.id.btn_main_activity_2);
+        final Button btnMainActivity3 = (Button) findViewById(R.id.btn_main_activity_3);
 
         btnStartActivity.setOnClickListener(this);
         btnMainActivity2.setOnClickListener(this);
         btnMainActivity3.setOnClickListener(this);
 
-//        btnStartActivity.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Log.d(MainActivity.TAG,"Button Clicked");
-//                startSecondActivity(v);
-//            }
-//        });
-//
-//        btnMainActivity2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Log.d(MainActivity.TAG,"Button 2 Clicked");
-//            }
-//        });
-//
-//        btnMainActivity3.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Log.d(MainActivity.TAG,"Button 3 Clicked");
-//            }
-//        });
+        this.mTvInfo = (TextView) findViewById(R.id.tv_info_main_activity);
 
-        Toast.makeText(this, "I'am Created", Toast.LENGTH_SHORT).show();
-        Log.d(MainActivity.TAG, "onCreate()");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        Toast.makeText(this, "I'am Started", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "I'am Started", Toast.LENGTH_SHORT).show();
 
     }
 
@@ -71,7 +48,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     protected void onResume() {
         super.onResume();
 
-        Toast.makeText(this, "I'am Resumed", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "I'am Resumed", Toast.LENGTH_SHORT).show();
 
     }
 
@@ -79,7 +56,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     protected void onPause() {
         super.onPause();
 
-        Toast.makeText(this, "I'am Pauzed", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "I'am Pauzed", Toast.LENGTH_SHORT).show();
 
     }
 
@@ -87,7 +64,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     protected void onStop() {
         super.onStop();
 
-        Toast.makeText(this, "I'am Stoped", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "I'am Stoped", Toast.LENGTH_SHORT).show();
 
     }
 
@@ -95,7 +72,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     protected void onDestroy() {
         super.onDestroy();
 
-        Toast.makeText(this, "I'am Destroyed", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "I'am Destroyed", Toast.LENGTH_SHORT).show();
 
     }
 
@@ -103,14 +80,16 @@ public class MainActivity extends Activity implements View.OnClickListener {
     protected void onRestart() {
         super.onRestart();
 
-        Toast.makeText(this, "I'am Restart", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "I'am Restart", Toast.LENGTH_SHORT).show();
 
     }
 
     public void startSecondActivity(View v) {
 
         Intent activityIntent = new Intent(this, SecondActivity.class);
-        startActivity(activityIntent);
+        //startActivity(activityIntent);
+
+        startActivityForResult(activityIntent, 0);
 
     }
 
@@ -125,9 +104,27 @@ public class MainActivity extends Activity implements View.OnClickListener {
             Log.d("OnclickClass", "Button2 Clicked");
             startSecondActivity(v);
 
+
         } else if (v.getId() == R.id.btn_main_activity_3 ) {
             Log.w("OnclickClass", "Button3 Clicked");
 
         }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.w(MainActivity.TAG, "Get activity result ");
+
+        if (requestCode == 0 && resultCode ==  Activity.RESULT_OK) {
+            String returnData = data.getStringExtra("myData");
+            Log.i(MainActivity.TAG, returnData);
+
+            this.mTvInfo.setText(returnData);
+
+        }
+
+    }
+
 }
--- app/src/main/java/org/bts/atry/lession4/SecondActivity.java
+++ app/src/main/java/org/bts/atry/lession4/SecondActivity.java
@@ -1,13 +1,43 @@
 package org.bts.atry.lession4;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = SecondActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button btnStartActivity = (Button) findViewById(R.id.btn_return_info_second_activity);
+
+        btnStartActivity.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.btn_return_info_second_activity) {
+
+            Log.e(SecondActivity.TAG,"Button second activity is being clicked");
+
+            Intent returnIntent = new Intent();
+            returnIntent.putExtra("myData","Hello World from second activity");
+
+            setResult(Activity.RESULT_OK, returnIntent);
+
+            finish();
+
+        }
+
     }
 }
